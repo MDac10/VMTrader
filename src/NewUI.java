e87710d2114954ade69cdfcb2c65e5914b0ea872
MDac10 <76761948+MDac10@users.noreply.github.com>
2022-03-30 19:17:17 -0400

    Update NewUI.java

--- src/NewUI.java
+++ src/NewUI.java
@@ -1,239 +1,271 @@
-package cryptoTrader.utils;
+package cryptoTrader.gui;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.lang.reflect.Array;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Vector;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFormattedTextField.AbstractFormatter;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CategoryAxis;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.axis.LogAxis;
-import org.jfree.chart.plot.CategoryPlot;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.category.BarRenderer;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.chart.renderer.xy.XYSplineRenderer;
-import org.jfree.data.Range;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.time.Day;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
 
-import cryptoTrader.gui.MainUI;
-import cryptoTrader.gui.NewUI;
+import cryptoTrader.utils.DataVisualizationCreator;
+//import cryptoTrader.utils.AbstractFormatter;
 
-public class DataVisualizationCreator {
+public class NewUI extends JFrame implements ActionListener{
+
+	private static final long serialVersionUID = 1L;
+
+	private static NewUI instance;
+	private JPanel stats, chartPanel, tablePanel;
+
+	// Should be a reference to a separate object in actual implementation
+	private List<String> selectedList;
+
+	private JTextArea selectedTickerList;
+//	private JTextArea tickerList;
+	private JTextArea tickerText;
+	private JTextArea BrokerText;
+	private JComboBox<String> strategyList;
+	private Map<String, List<String>> brokersTickers = new HashMap<>();
+	private Map<String, String> brokersStrategies = new HashMap<>();
+	private List<String> selectedTickers = new ArrayList<>();
+	private String selectedStrategy = "";
+	private DefaultTableModel dtm;
+	private JTable table;
 	
-	public void createCharts() {
-//		createTextualOutput();
-		createTableOutput();
-//		createTimeSeries();
-//		createScatter();
-		createBar();
+	private static Object[][] clientInfo;
+	private static int numRows;
+	
+	private String datePatern = "dd/MM/yyyy";
+	private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePatern);
+
+	public static NewUI getInstance() {
+		if (instance == null)
+			instance = new NewUI();
+
+		return instance;
 	}
 
-	private void createTextualOutput() {
-//		DefaultTableModel dtm = new  DefaultTableModel(new Object[] {"Broker Name", "Ticker List", "Strategy Name"}, 1);
-//		JTable table = new JTable(dtm);
-//		//table.setPreferredSize(new Dimension(600, 300));
-//		dtm.e
-//		JScrollPane scrollPane = new JScrollPane(table);
-//		scrollPane.setBorder (BorderFactory.createTitledBorder (BorderFactory.createEtchedBorder (),
-//                "Broker Actions",
-//                TitledBorder.CENTER,
-//                TitledBorder.TOP));
-//		
-//	
-//		
-//		scrollPane.setPreferredSize(new Dimension(800, 300));
-//		table.setFillsViewportHeight(true);;
-		
-//		MainUI.getInstance().updateStats(scrollPane);
-	}
-	
-	private void createTableOutput() {
-		// Dummy dates for demo purposes. These should come from selection menu
-		Object[] columnNames = {"Trader","Strategy","CryptoCoin","Action","Quantity","Price","Date"};
-		
-		Object[][] rawData = NewUI.getInstance().getClientInfo();
-		
-		for(int i = 0; i < NewUI.getInstance().getNumRows(); i++) {
-			Object brokerName = rawData[i][0];
-			Object strategyName = rawData[i][2];
-			String[] coinName = rawData[i][1].toString().split(","); 
-			for(int j = 0; j < Array.getLength(coinName); j++) {
-				Object currCoin = coinName[j];
-			}
-			
-		}
-		
-		Object[][] data = {};
+	private NewUI() {
 
-		JTable table = new JTable(data, columnNames);
-		//table.setPreferredSize(new Dimension(600, 300));
+		// Set window title
+		super("test window");
+
+		// Set top bar
+
+
+		JPanel north = new JPanel();
+
+//		north.add(strategyList);
+
+		// Set bottom bar
+		JLabel from = new JLabel("From");
+		UtilDateModel dateModel = new UtilDateModel();
+		Properties p = new Properties();
+		p.put("text.today", "Today");
+		p.put("text.month", "Month");
+		p.put("text.year", "Year");
+		JDatePanelImpl datePanel = new JDatePanelImpl(dateModel, p);
 		
+//		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new AbstractFormatter());
+//		@SuppressWarnings("serial")
+//		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new AbstractFormatter() {
+//			private String datePatern = "dd/MM/yyyy";
+//
+//			private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePatern);
+//
+//			@Override
+//			public Object stringToValue(String text) throws ParseException {
+//				return dateFormatter.parseObject(text);
+//			}
+//
+//			@Override
+//			public String valueToString(Object value) throws ParseException {
+//				if (value != null) {
+//					Calendar cal = (Calendar) value;
+//					return dateFormatter.format(cal.getTime());
+//				}
+//
+//				return "";
+//			}
+//		});
+
+		JButton trade = new JButton("Perform Trade");
+		trade.setActionCommand("trade");
+		trade.addActionListener(this);
+
+
+
+		JPanel south = new JPanel();
+		
+		south.add(trade);
+
+		dtm = new DefaultTableModel(new Object[] { "Trading Client", "Coin List", "Strategy Name" }, 1);
+		table = new JTable(dtm);
+		// table.setPreferredSize(new Dimension(600, 300));
 		JScrollPane scrollPane = new JScrollPane(table);
-		scrollPane.setBorder (BorderFactory.createTitledBorder (BorderFactory.createEtchedBorder (),
-                "Trader Actions",
-                TitledBorder.CENTER,
-                TitledBorder.TOP));
-		
-	
-		
+		scrollPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Trading Client Actions",
+				TitledBorder.CENTER, TitledBorder.TOP));
+		Vector<String> strategyNames = new Vector<String>();
+		strategyNames.add("None");
+		strategyNames.add("Strategy-A");
+		strategyNames.add("Strategy-B");
+		strategyNames.add("Strategy-C");
+		strategyNames.add("Strategy-D");
+		TableColumn strategyColumn = table.getColumnModel().getColumn(2);
+		JComboBox comboBox = new JComboBox(strategyNames);
+		strategyColumn.setCellEditor(new DefaultCellEditor(comboBox));
+		JButton addRow = new JButton("Add Row");
+		JButton remRow = new JButton("Remove Row");
+		addRow.setActionCommand("addTableRow");
+		addRow.addActionListener(this);
+		remRow.setActionCommand("remTableRow");
+		remRow.addActionListener(this);
+
 		scrollPane.setPreferredSize(new Dimension(800, 300));
-		table.setFillsViewportHeight(true);;
+		table.setFillsViewportHeight(true);
 		
-		//MainUI.getInstance().updateStats(scrollPane);
-		NewUI.getInstance().updateStats(scrollPane);
+
+		JPanel east = new JPanel();
+//		east.setLayout();
+		east.setLayout(new BoxLayout(east, BoxLayout.Y_AXIS));
+//		east.add(table);
+		east.add(scrollPane);
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new BoxLayout(buttons, BoxLayout.X_AXIS));
+		buttons.add(addRow);
+		buttons.add(remRow);
+		east.add(buttons);
+//		east.add(selectedTickerListLabel);
+//		east.add(selectedTickersScrollPane);
+
+		// Set charts region
+		JPanel west = new JPanel();
+		west.setPreferredSize(new Dimension(1250, 650));
+		stats = new JPanel();
+		stats.setLayout(new GridLayout(2, 2));
+
+		west.add(stats);
+
+		getContentPane().add(north, BorderLayout.NORTH);
+		getContentPane().add(east, BorderLayout.EAST);
+		getContentPane().add(west, BorderLayout.CENTER);
+		getContentPane().add(south, BorderLayout.SOUTH);
+//		getContentPane().add(west, BorderLayout.WEST);
 	}
 
-	private void createTimeSeries() {
-		TimeSeries series1 = new TimeSeries("Bitcoin - Daily");
-		series1.add(new Day(13, 9, 2021), 50368.67);
-		series1.add(new Day(14, 9, 2021), 51552.05);
-		series1.add(new Day(15, 9, 2021), 47228.30);
-		series1.add(new Day(16, 9, 2021), 45263.90);
-		series1.add(new Day(17, 9, 2021), 46955.41);
-		
-		TimeSeries series2 = new TimeSeries("Ethereum - Daily");
-		series2.add(new Day(13, 9, 2021), 3912.28);
-		series2.add(new Day(14, 9, 2021), 3927.27);
-		series2.add(new Day(15, 9, 2021), 3460.48);
-		series2.add(new Day(16, 9, 2021), 3486.09);
-		series2.add(new Day(17, 9, 2021), 3550.29);
-
-		TimeSeries series3 = new TimeSeries("Cardano - Daily");
-		series3.add(new Day(13, 9, 2021), 2.87);
-		series3.add(new Day(14, 9, 2021), 2.84);
-		series3.add(new Day(15, 9, 2021), 2.41);
-		series3.add(new Day(16, 9, 2021), 2.43);
-		series3.add(new Day(17, 9, 2021), 2.59);
-
-		TimeSeriesCollection dataset = new TimeSeriesCollection();
-		dataset.addSeries(series1);
-		dataset.addSeries(series2);
-		dataset.addSeries(series3);
-
-		XYPlot plot = new XYPlot();
-		XYSplineRenderer splinerenderer1 = new XYSplineRenderer();
-		
-		plot.setDataset(0, dataset);
-		plot.setRenderer(0, splinerenderer1);
-		DateAxis domainAxis = new DateAxis("");
-		plot.setDomainAxis(domainAxis);
-		plot.setRangeAxis(new LogAxis("Price(USD)"));
-
-		//plot.mapDatasetToRangeAxis(0, 0);// 1st dataset to 1st y-axis
-		//plot.mapDatasetToRangeAxis(1, 1); // 2nd dataset to 2nd y-axis
-		//plot.mapDatasetToRangeAxis(2, 2);// 3rd dataset to 3rd y-axis
-		
-		JFreeChart chart = new JFreeChart("Daily Price Line Chart", new Font("Serif", java.awt.Font.BOLD, 18), plot,
-				true);
-
-		ChartPanel chartPanel = new ChartPanel(chart);
-		chartPanel.setPreferredSize(new Dimension(800, 300));
-		chartPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		chartPanel.setBackground(Color.white);
-		
-		//MainUI.getInstance().updateStats(chartPanel);
-		NewUI.getInstance().updateStats(chartPanel);
+	public void updateStats(JComponent component) {
+		stats.add(component);
+		stats.revalidate();
 	}
 	
-	private void createScatter() {
-		TimeSeries series1 = new TimeSeries("Bitcoin - Daily");
-		series1.add(new Day(13, 9, 2021), 50368.67);
-		series1.add(new Day(14, 9, 2021), 51552.05);
-		series1.add(new Day(15, 9, 2021), 47228.30);
-		series1.add(new Day(16, 9, 2021), 45263.90);
-		series1.add(new Day(17, 9, 2021), 46955.41);
-		
-		TimeSeries series2 = new TimeSeries("Ethereum - Daily");
-		series2.add(new Day(13, 9, 2021), 3912.28);
-		series2.add(new Day(14, 9, 2021), 3927.27);
-		series2.add(new Day(15, 9, 2021), 3460.48);
-		series2.add(new Day(16, 9, 2021), 3486.09);
-		series2.add(new Day(17, 9, 2021), 3550.29);
-
-		TimeSeries series3 = new TimeSeries("Cardano - Daily");
-		series3.add(new Day(13, 9, 2021), 2.87);
-		series3.add(new Day(14, 9, 2021), 2.84);
-		series3.add(new Day(15, 9, 2021), 2.41);
-		series3.add(new Day(16, 9, 2021), 2.43);
-		series3.add(new Day(17, 9, 2021), 2.59);
-
-		TimeSeriesCollection dataset = new TimeSeriesCollection();
-		dataset.addSeries(series1);
-		dataset.addSeries(series2);
-		dataset.addSeries(series3);
-
-		XYPlot plot = new XYPlot();
-		XYItemRenderer itemrenderer1 = new XYLineAndShapeRenderer(false, true);
-
-		plot.setDataset(0, dataset);
-		plot.setRenderer(0, itemrenderer1);
-		DateAxis domainAxis = new DateAxis("");
-		plot.setDomainAxis(domainAxis);
-		plot.setRangeAxis(new LogAxis("Price(USD)"));
-
-		//plot.mapDatasetToRangeAxis(0, 0);// 1st dataset to 1st y-axis
-		//plot.mapDatasetToRangeAxis(1, 1); // 2nd dataset to 2nd y-axis
-		
-		JFreeChart scatterChart = new JFreeChart("Daily Price Scatter Chart",
-				new Font("Serif", java.awt.Font.BOLD, 18), plot, true);
-
-		ChartPanel chartPanel = new ChartPanel(scatterChart);
-		chartPanel.setPreferredSize(new Dimension(600, 300));
-		chartPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		chartPanel.setBackground(Color.white);
-		
-		//MainUI.getInstance().updateStats(chartPanel);
-		NewUI.getInstance().updateStats(chartPanel);
+	public Object[][] getClientInfo() {
+		return clientInfo;
 	}
 	
-	private void createBar() {
+	public int getNumRows() {
+		return numRows;
+	}
+
+	public static void main(String[] args) {
+		JFrame frame = NewUI.getInstance();
+		frame.setSize(900, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-//		Those are hard-coded values!!!! 
-//		You will have to come up with a proper datastructure to populate the BarChart with live data!
-		dataset.setValue(6, "Trader-1", "Strategy-A");
-		dataset.setValue(5, "Trader-2", "Strategy-B");
-		dataset.setValue(0, "Trader-3", "Strategy-E");
-		dataset.setValue(1, "Trader-4", "Strategy-C");
-		dataset.setValue(10, "Trader-5", "Strategy-D");
-
-		CategoryPlot plot = new CategoryPlot();
-		BarRenderer barrenderer1 = new BarRenderer();
-
-		plot.setDataset(0, dataset);
-		plot.setRenderer(0, barrenderer1);
-		CategoryAxis domainAxis = new CategoryAxis("Strategy");
-		plot.setDomainAxis(domainAxis);
-		LogAxis rangeAxis = new LogAxis("Actions(Buys or Sells)");
-		rangeAxis.setRange(new Range(1.0, 20.0));
-		plot.setRangeAxis(rangeAxis);
-
-		//plot.mapDatasetToRangeAxis(0, 0);// 1st dataset to 1st y-axis
-		//plot.mapDatasetToRangeAxis(1, 1); // 2nd dataset to 2nd y-axis
-
-		JFreeChart barChart = new JFreeChart("Actions Performed By Traders So Far", new Font("Serif", java.awt.Font.BOLD, 18), plot,
-				true);
-
-		ChartPanel chartPanel = new ChartPanel(barChart);
-		chartPanel.setPreferredSize(new Dimension(600, 300));
-		chartPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		chartPanel.setBackground(Color.white);
+		String command = e.getActionCommand();
 		
-		//MainUI.getInstance().updateStats(chartPanel);
-		NewUI.getInstance().updateStats(chartPanel);
+		if ("trade".equals(command)) {
+			
+			clientInfo = new Object[dtm.getRowCount()][3];
+			numRows = dtm.getRowCount();
+			
+			for (int count = 0; count < dtm.getRowCount(); count++){
+				
+					Object traderObject = dtm.getValueAt(count, 0);
+					
+					if (traderObject == null) {
+						JOptionPane.showMessageDialog(this, "please fill in Trader name on line " + (count + 1) );
+						return;
+					}
+					
+					String traderName = traderObject.toString();
+					
+					Object coinObject = dtm.getValueAt(count, 1);
+					
+					if (coinObject == null) {
+						JOptionPane.showMessageDialog(this, "please fill in cryptocoin list on line " + (count + 1) );
+						return;
+					}
+					
+					String[] coinNames = coinObject.toString().split(",");
+					
+					Object strategyObject = dtm.getValueAt(count, 2);
+					
+					if (strategyObject == null) {
+						JOptionPane.showMessageDialog(this, "please fill in strategy name on line " + (count + 1) );
+						return;
+					}
+					
+					String strategyName = strategyObject.toString();
+					
+					clientInfo[count][0] = traderObject;
+					clientInfo[count][1] = coinNames;
+					clientInfo[count][2] = strategyObject;
+					
+					System.out.println("initial: " + traderName + " " + Arrays.toString(coinNames) + " " + strategyName);
+					System.out.println("new: " + clientInfo[count][0] + " " + Arrays.toString((Object[]) clientInfo[count][1]) + " " + clientInfo[count][2]);
+	        }
+			
+			stats.removeAll();
+			DataVisualizationCreator creator = new DataVisualizationCreator();
+			creator.createCharts();
+			
+		} else if ("addTableRow".equals(command)) {
+			
+			//adds a new row to the table on the right side of the window "Trader Actions"
+			dtm.addRow(new String[3]);
+			
+		} else if ("remTableRow".equals(command)) {
+			
+			int selectedRow = table.getSelectedRow();
+			if (selectedRow != -1)
+				dtm.removeRow(selectedRow);
+		}
 	}
 
 }
