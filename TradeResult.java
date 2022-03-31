487e6d267be00b8c2fbdb35544b6f1f365a3e1d2
MDac10 <76761948+MDac10@users.noreply.github.com>
2022-03-31 13:40:39 -0400

    Delete TradeResult.java

--- TradeResult.java
+++
@@ -1,8 +0,0 @@
-package cryptoTrader.utils;
-
-//All strategies will implement this interface when deciding their trade strategy
-public abstract class TradeResult {
-	
-	public abstract String trade(String[] coinList, double[] coinPriceList);
-
-}
