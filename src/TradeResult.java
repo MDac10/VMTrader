9809b56f4599ae0bf1671322e1a526eab57855af
vitowong0 <101366834+vitowong0@users.noreply.github.com>
2022-04-04 13:41:25 -0400

    Updates w Meg
    Follow this structure to return the correct objects

--- src/TradeResult.java
+++ src/TradeResult.java
@@ -2,23 +2,37 @@ package cryptoTrader.utils;
 
 //Data structure to hold the data necessary for graphs
 public class TradeResult {
-	
-	static class Trade{
-		Object traderName;
-		int numOfStrats;
-		
-		public Trade(Object traderName, int numOfStrats) {
-			this.traderName = traderName;
-			this.numOfStrats = numOfStrats;
-		}
-		
-		public int getNumOfStrats() {
-			return numOfStrats;
-		}
-		
-		public Object getName() {
-			return traderName;
-		}
+
+//	static class Trade{
+	// i want it to return uhhh
+	// name, action, qty
+	String name;
+	String action;
+	int qty;
+
+	Object traderName;
+	int numOfStrats;
+
+	public int getNumOfStrats() {
+		return numOfStrats;
+	}
+
+	public Object getName() {
+		return traderName;
+	}
+//	}
+
+	private Object[][] output;
+
+	public void trade(int nRows, Object[][] array) {
+		// TODO Auto-generated method stub
+		output = new Object[nRows][4];
+		output = array;
+
+	}
+
+	public Object[][] getArray() {
+		return output;
 	}
 
 }
